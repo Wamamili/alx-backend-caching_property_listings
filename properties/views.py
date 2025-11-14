c5089f7a34c5bf435372f52f7a2a7eec2db4d444
wamamili <wamamiliclean@gmail.com>
2025-11-14 11:29:44 +0300

    commit

--- properties/views.py
+++ properties/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-from django.views.decorators.cache import cache_page
-from .models import Property
-from django.core.cache import cache
+from .utils import get_all_properties
 
-@cache_page(60 * 15)  # Cache response for 15 minutes
 def property_list(request):
-    cache_key = "all_properties"
-    properties = cache.get(cache_key)
-    
-    if not properties:
-        properties = list(Property.objects.all().values("id", "title", "description", "price", "location", "created_at"))
-        cache.set(cache_key, properties, timeout=60 * 15)  # Cache for 15 minutes
-    
-    return JsonResponse(properties, safe=False)
+    properties = get_all_properties()
+
+    data = [
+        {
+            "id": item.id,
+            "title": item.title,
+            "description": item.description,
+            "price": str(item.price),
+            "location": item.location,
+            "created_at": item.created_at.isoformat()
+        }
+        for item in properties
+    ]
+
+    return JsonResponse(data, safe=False)
